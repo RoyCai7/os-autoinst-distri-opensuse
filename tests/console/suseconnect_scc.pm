96e0ac820c894dfc38a0ea9aa5447df73ce360fa
Roy Cai <roy.cai@suse.com>
2025-12-22 12:41:30 +0800

    Fix hyperv serial after snapshot
    test

--- tests/console/suseconnect_scc.pm
+++ tests/console/suseconnect_scc.pm
@@ -10,8 +10,34 @@
 use Mojo::Base qw(consoletest);
 use testapi;
 use registration qw(verify_scc investigate_log_empty_license runtime_registration);
+use backend::console_proxy;
+
+# Recover serial console after Hyper-V snapshot restore
+# Serial console may become unresponsive after VM snapshot restore on Hyper-V
+sub recover_serial_console_hyperv {
+    return unless check_var('HYPERV', '1') || check_var('VIRSH_VMM_FAMILY', 'hyperv');
+
+    # Test if serial console is responsive
+    my $serial_ok = eval {
+        script_run('true', timeout => 10);
+        1;
+    };
+
+    return if $serial_ok;
+
+    record_info('Serial Recovery', 'Serial console unresponsive after snapshot restore, attempting recovery...');
+
+    # Use VNC/graphical console to restart serial service
+    select_console('root-console');
+    enter_cmd('systemctl restart serial-getty@ttyS0.service');
+    sleep 5;
+
+    # Re-select serial terminal
+    select_console('root-console');
+}
 
 sub run {
+    recover_serial_console_hyperv();
     runtime_registration();    # assume it will run in serial terminal
 }
 
